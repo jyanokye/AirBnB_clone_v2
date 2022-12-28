1e0dddf678a29eee24f15f38f5b162892956a29c
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2022-12-28 15:22:23 +0000

    Update test_db_storage.py

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,152 +1,71 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""
-"""
-import sys
 import unittest
-import inspect
-import io
-import pep8
+import os.path
+import os
 from datetime import datetime
-from contextlib import redirect_stdout
-from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
-from models import storage
-from models.user import User
+from models.engine.db_storage import DBStorage
+from models import *
+from console import HBNBCommand
 
 
-class TestFileStorage(unittest.TestCase):
+@unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE', '') != "db", "db")
+class Test_DBStorage(unittest.TestCase):
     """
-    class for testing FileStorage class' methods
+    Test the db storage class
     """
-    temp_file = ""
-
-    @classmethod
-    def setUpClass(cls):
-        """
-        Set up class method for the doc tests
-        """
-        cls.setup = inspect.getmembers(FileStorage, inspect.isfunction)
-
-    def test_pep8_conformance_FileStorage(self):
-        """
-        Test that file_storage.py file conform to PEP8
-        """
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/file_storage.py'])
-        self.assertEqual(result.total_errors, 1,
-                         "Found code style errors (and warnings).")
-
-    def test_pep8_conformance_test_FileStorage(self):
-        """
-        Test that test_file_storage.py file conform to PEP8
-        """
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['tests/test_models/\
-                                        test_file_storage.py'])
-        self.assertEqual(result.total_errors, 1,
-                         "Found code style errors (and warnings).")
-
-    def test_module_docstring(self):
-        """
-        Tests if module docstring documentation exist
-        """
-        self.assertTrue(len(FileStorage.__doc__) >= 1)
-
-    def test_class_docstring(self):
-        """
-        Tests if class docstring documentation exist
-        """
-        self.assertTrue(len(FileStorage.__doc__) >= 1)
-
-    def test_func_docstrings(self):
-        """
-        Tests if methods docstring documntation exist
-        """
-        for func in self.setup:
-            self.assertTrue(len(func[1].__doc__) >= 1)
-
-    @staticmethod
-    def move_file(src, dest):
-        with open(src, 'r', encoding='utf-8') as myFile:
-            with open(dest, 'w', encoding='utf-8') as tempFile:
-                tempFile.write(myFile.read())
-        os.remove(src)
 
     def setUp(self):
-        self.temp_file = '/temp_store.json'
-        self.temp_objs = [BaseModel(), BaseModel(), BaseModel()]
-        for obj in self.temp_objs:
-            storage.new(obj)
-        storage.save()
+        self.cli = HBNBCommand()
+        self.store = DBStorage()
 
-    def tearDown(self):
-        """initialized object
-        """
-        del self.temp_objs
+        test_args = {'updated_at': datetime(2017, 2, 12, 00, 31, 53, 331997),
+                     'id': 'f519fb40-1f5c-458b-945c-2ee8eaaf4900',
+                     'created_at': datetime(2017, 2, 12, 00, 31, 53, 331900),
+                     'name': 'wifi'}
+        self.model = Amenity(test_args)
 
-    def test_type(self):
-        """type checks for FileStorage
-        """
-        self.assertIsInstance(storage, FileStorage)
-        self.assertEqual(type(storage), FileStorage)
+    def test_init(self):
+        self.assertNotEqual(self.store._DBStorage__engine, None)
+        self.assertEqual(self.store._DBStorage__session, None)
+
+    def test_all(self):
+        self.store.reload()
+
+        total = len(self.store.all())
+        self.model.save()
+        self.assertEqual(total + 1, len(self.store.all()))
+
+        self.cli.do_create('User email="some@gmail.com" password="hello"')
+        self.assertEqual(total + 2, len(self.store.all()))
+
+    def test_all_class_specified(self):
+        self.store.reload()
+
+        total = len(self.store.all("Amenity"))
+        self.model.save()
+        self.assertEqual(total + 1, len(self.store.all("Amenity")))
+
+        self.cli.do_create('Amenity name="bathroom"')
+        self.assertEqual(total + 2, len(self.store.all("Amenity")))
+
+    def test_all_invalid_class(self):
+        self.store.reload()
+
+        total = len(self.store.all())
+        storage = self.store.all("Dog")
+
+        self.assertTrue(storage == None)
+        self.assertEqual(total, len(self.store.all()))
+
+    def test_new(self):
+        pass
 
     def test_save(self):
-        """tests save functionality for FileStorage
-        """
-        with open('file.json', 'r', encoding='utf-8') as myFile:
-            dump = myFile.read()
-        self.assertNotEqual(len(dump), 0)
-        temp_d = eval(dump)
-        key = self.temp_objs[0].__class__.__name__ + '.'
-        key += str(self.temp_objs[0].id)
-        self.assertNotEqual(len(temp_d[key]), 0)
-        key2 = 'State.412409120491902491209491024'
-        try:
-            self.assertRaises(temp_d[key2], KeyError)
-        except:
-            pass
+        pass
 
     def test_reload(self):
-        """tests reload functionality for FileStorage
-        """
-        storage.reload()
-        obj_d = storage.all()
-        key = self.temp_objs[1].__class__.__name__ + '.'
-        key += str(self.temp_objs[1].id)
-        self.assertNotEqual(obj_d[key], None)
-        self.assertEqual(obj_d[key].id, self.temp_objs[1].id)
-        key2 = 'State.412409120491902491209491024'
-        try:
-            self.assertRaises(obj_d[key2], KeyError)
-        except:
-            pass
+        self.store.reload()
+        self.assertNotEqual(self.store._DBStorage__session, None)
 
-    def test_delete_basic(self):
-        """tests delete basic functionality for FileStorage
-        """
-        obj_d = storage.all()
-        key2 = self.temp_objs[2].__class__.__name__ + '.'
-        key2 += str(self.temp_objs[2].id)
-        try:
-            self.assertRaises(obj_d[key2], KeyError)
-        except:
-            pass
 
-    def test_new_basic(self):
-        """tests new basic functionality for FileStorage
-        """
-        obj = BaseModel()
-        storage.new(obj)
-        obj_d = storage.all()
-        key = obj.__class__.__name__ + '.' + str(obj.id)
-        self.assertEqual(obj_d[key] is obj, True)
-
-    def test_new_badinput(self):
-        """tests new bad input functionality for FileStorage
-        """
-        try:
-            self.assertRaises(storage.new('jwljfef'), TypeError)
-            self.assertRaises(storage.new(None), TypeError)
-        except:
-            pass
+if __name__ == "__main__":
+    unittest.main()
